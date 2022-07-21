152392475c2e237951ec2f37ddbd64905f087265
Zukey Ramirez <zramirez18320@plataforma-utslp.net>
2022-07-20 20:42:47 -0500

    Segundo avance

---
+++ js/acciones.js
@@ -0,0 +1,9 @@
+alert('hi');
+
+function suma() {
+var num1 = parseFloat (document.getElementById('numero1').value) ;
+var num2 = parseFloat(document.getElementById('numero2').value) ;
+var resul = num1 + num2;
+document.getElementById('resultado').value = resul;
+}
+
